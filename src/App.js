7bcfa31b3a35a842c410d063ea51d9dcf1055588
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-06-11 20:23:32 +0100

    Added awesome fonts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 // import React from 'react';
+import { useState } from "react"
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 // You can only return 1 parent element within a function
 // We can perform JS operations within the function
 
 function App() {
 
+  const [tasks, setTasks] = useState(
+    [{
+      id: 1,
+      text: "Code",
+      day: "2Day",
+      reminder: false,
+    },
+  ]
+);
+
   return (
     <div className="container">
-      <Header title='Hello'/>
+      <Header/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import PropTypes from 'prop-types';
+import Button from "./Button";
 
-const Header = () => {
+const Header = ({ title }) => {
+
+    const onClick = () => {
+        console.log('object');
+    }
+    
   return (
-    <header>
-        <h1>Task Tracker</h1>
+    <header className='header'>
+        <h1>{title}</h1>
+        <Button color='green' text='Add' onClick={onClick}/>
     </header>
   )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
 }
 
 Header.propTypes = {
